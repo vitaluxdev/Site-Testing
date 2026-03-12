8f9c013d6f462ab71aeddcd3b4f111f97b385944
Vitalux <93072230+vitaluxdev@users.noreply.github.com>
2026-03-12 18:47:48 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
-}
-
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
-}
+document.getElementById("disabled").disabled = true;
