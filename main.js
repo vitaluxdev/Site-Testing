e45c7156e41fdc537e0522dc7ce01a99e9c72a5d
Vitalux <93072230+vitaluxdev@users.noreply.github.com>
2026-03-14 02:55:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-document.getElementById("disabled").disabled = true;
+function pageHome() {
+    var sections = ["home", "friends", "projects"];
+    var buttons = ["buttonHome", "buttonFriends", "buttonProjects"];
+
+    sections.forEach((id, index) => {
+        var section = document.getElementById(id);
+        var button = document.getElementById(buttons[index]);
+
+        if (id === "home") {
+            button.setAttribute('class', 'pageButton-active');
+        } else {
+            button.setAttribute('class', 'pageButton');
+        }
+    });
+}
+
+function pageFriends() {
+    var sections = ["home", "friends", "projects"];
+    var buttons = ["buttonHome", "buttonFriends", "buttonProjects"];
+
+    sections.forEach((id, index) => {
+        var section = document.getElementById(id);
+        var button = document.getElementById(buttons[index]);
+
+        if (id === "friends") {
+            button.setAttribute('class', 'pageButton-active');
+        } else {
+            button.setAttribute('class', 'pageButton');
+        }
+    });
+}
+
+function pageProjects() {
+    var sections = ["home", "friends", "projects"];
+    var buttons = ["buttonHome", "buttonFriends", "buttonProjects"];
+
+    sections.forEach((id, index) => {
+        var section = document.getElementById(id);
+        var button = document.getElementById(buttons[index]);
+
+        if (id === "projects") {
+            button.setAttribute('class', 'pageButton-active');
+        } else {
+            button.setAttribute('class', 'pageButton');
+        }
+    });
+}
